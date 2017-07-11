35fb3ce540d779d2ec6968dcce46b81f95bbcc48
0xelectron <0xproton@gmail.com>
2017-07-12 00:47:15 +0530

    Added help text to all fields

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 from django.core.validators import RegexValidator
 from base.models import (Address, Center, Participant)
 
@@ -13,18 +14,23 @@ class Event(models.Model):
     ONLY_DIGITS_VALIDATOR = RegexValidator(regex=r'^[0-9]*$',
                                 message="Only digits allowed.")
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, help_text=_("Event Name"))
     venue = models.ForeignKey(Address, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    last_date_of_registration = models.DateField()
-    fees = models.DecimalField(max_digits=10, decimal_places=2)
-    late_fees = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(help_text=_("Event Start Date"))
+    end_date = models.DateField(help_text=_("Event End Date"))
+    last_date_of_registration = models.DateField(
+                                help_text=_("Last Date of Registration"))
+    fees = models.DecimalField(max_digits=10, decimal_places=2,
+                                help_text=_("Event Fees"))
+    late_fees = models.DecimalField(max_digits=10, decimal_places=2,
+                                help_text=_("Late Registration Fees"))
 
     # This represents Age group
-    min_age = models.CharField(max_length=2, validators=[ONLY_DIGITS_VALIDATOR,])
-    max_age = models.CharField(max_length=2, validators=[ONLY_DIGITS_VALIDATOR,])
-    remarks = models.TextField()
+    min_age = models.CharField(max_length=2, validators=[ONLY_DIGITS_VALIDATOR,],
+                                help_text=_("Age Group Lower limit"))
+    max_age = models.CharField(max_length=2, validators=[ONLY_DIGITS_VALIDATOR,],
+                                help_text=_("Age Group Upper limit"))
+    remarks = models.TextField(help_text=_("Any remarks"))
 
 class EventParticipant(models.Model):
     """EventParticipant stores information about an participant for the Event.
@@ -52,15 +58,17 @@ class EventParticipant(models.Model):
 
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     pariticipant = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    registration_no = models.CharField(max_length=20, validators=[ONLY_DIGITS_VALIDATOR,])
-    home_center = models.ForeignKey(Center, on_delete=models.CASCADE, related_name='home_center')
-    event_center = models.ForeignKey(Center, on_delete=models.CASCADE, blank=True,
-                                    null=True, related_name='event_center')
-    accommodation = models.BooleanField()
-    payment_status = models.BooleanField()
-    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
-    cashier = models.CharField(max_length=50)
-    role = models.CharField(max_length=12, choices=ROLE_CHOICES)
+    registration_no = models.CharField(max_length=20, validators=[ONLY_DIGITS_VALIDATOR,],
+                                        help_text=_("Registration Number"))
+    home_center = models.ForeignKey(Center, on_delete=models.CASCADE, related_name='home_center',
+                                    help_text=_("Home Center"))
+    event_center = models.ForeignKey(Center, on_delete=models.CASCADE, blank=True, null=True,
+                                    related_name='event_center', help_text=_("Event Center"))
+    accommodation = models.BooleanField(help_text=_("Is accommodation required?"))
+    payment_status = models.BooleanField(help_text=_("Has paid?"))
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, help_text=_("Amount paid"))
+    cashier = models.CharField(max_length=50, help_text=_("Cashier"))
+    role = models.CharField(max_length=12, choices=ROLE_CHOICES, help_text=_("Role"))
 
 
 
