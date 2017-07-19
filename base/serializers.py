bdce7c8ec478423fcdaa7b49aec0b8c15f32b853
0xelectron <0xproton@gmail.com>
2017-07-19 14:26:11 +0530

    Initial API's for base models. No filtering yet

---
+++ base/serializers.py
@@ -0,0 +1,52 @@
+from base.models import (Address,
+                        Center,
+                        Participant,
+                        Profile)
+from rest_framework.serializers import ModelSerializer
+
+
+
+class CenterSerializer(ModelSerializer):
+    """CenterSerializer serializes the Center model
+    into json object and vice versa.
+    """
+
+    class Meta:
+        model = Center
+        exclude = ()
+
+
+
+class AddressSerializer(ModelSerializer):
+    """AddressSerializer serializes the Address model
+    into json object and vice versa.
+    """
+
+    class Meta:
+        model = Address
+        exclude = ()
+
+
+
+class ParticipantSerializer(ModelSerializer):
+    """PariticpantSerializer serializes the Participant model
+    into json object and vice versa.
+    """
+
+    class Meta:
+        model = Participant
+        exclude = ()
+
+
+
+class ProfileSerializer(ModelSerializer):
+    """ProfileSerializer serializes the Profile model
+    into json object and vice versa.
+    """
+
+    class Meta:
+        model = Profile
+        exclude = ()
+
+
+
---
+++ base/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
+from base.views import (CenterView,
+                        AddressView,
+                        ParticipantViewSet,
+                        ProfileViewSet)
+
+schema_view = get_schema_view(title='Base API')
+api_endpoints_default = {
+        'get': 'get',
+        'post': 'post',
+        'put': 'put',
+        'patch': 'patch',
+        }
+api_endpoints_list = {
+        'get': 'list',
+        }
+
+urlpatterns = [
+    url(r'^api-info/$', schema_view, name='api-info'),
+    url(r'^centers/$', CenterView.as_view(), name='centers'),
+    url(r'^addressess/(?P<pk>[0-9]+)/$', AddressView.as_view(),
+        name='addressess'),
+    url(r'^participants/(?P<pk>[0-9]+)/$', ParticipantViewSet.as_view(
+        api_endpoints_default), name='participants'),
+    url(r'^participants/list/$', ParticipantViewSet.as_view(
+        api_endpoints_list), name='participants-list'),
+    url(r'^profiles/(?P<pk>[0-9]+)/$', ProfileViewSet.as_view(
+        api_endpoints_default), name='profiles'),
+    url(r'^profiles/list/$', ProfileViewSet.as_view(api_endpoints_list),
+        name='profiles-list'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
