b52df904458d089c74554550890d2c6a3a0c75a9
0xelectron <0xproton@gmail.com>
2017-07-19 14:33:16 +0530

    Updated Url's Precedence

--- base/urls.py
+++ base/urls.py
@@ -22,14 +22,14 @@ urlpatterns = [
     url(r'^centers/$', CenterView.as_view(), name='centers'),
     url(r'^addressess/(?P<pk>[0-9]+)/$', AddressView.as_view(),
         name='addressess'),
-    url(r'^participants/(?P<pk>[0-9]+)/$', ParticipantViewSet.as_view(
-        api_endpoints_default), name='participants'),
     url(r'^participants/list/$', ParticipantViewSet.as_view(
         api_endpoints_list), name='participants-list'),
-    url(r'^profiles/(?P<pk>[0-9]+)/$', ProfileViewSet.as_view(
-        api_endpoints_default), name='profiles'),
+    url(r'^participants/(?P<pk>[0-9]+)/$', ParticipantViewSet.as_view(
+        api_endpoints_default), name='participants'),
     url(r'^profiles/list/$', ProfileViewSet.as_view(api_endpoints_list),
         name='profiles-list'),
+    url(r'^profiles/(?P<pk>[0-9]+)/$', ProfileViewSet.as_view(
+        api_endpoints_default), name='profiles'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
