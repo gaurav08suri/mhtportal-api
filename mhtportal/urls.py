e82596ec6566403f185cacaeb236e2a02200c1aa
0xelectron <0xproton@gmail.com>
2017-08-13 08:18:36 +0530

    Initial Authentication.

--- mhtportal/urls.py
+++ mhtportal/urls.py
@@ -17,6 +17,10 @@ from django.conf.urls import (url,
                             include)
 from django.contrib import admin
 from rest_framework.schemas import get_schema_view
+from rest_framework_jwt.views import (obtain_jwt_token,
+                                        refresh_jwt_token,
+                                        verify_jwt_token)
+
 
 schema_view = get_schema_view(title='Mht Portal APIs')
 
@@ -25,6 +29,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^base/', include('base.urls', namespace='base')),
     url(r'^events/', include('events.urls', namespace='events')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^api-info/$', schema_view, name='api-info'),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
