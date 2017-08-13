9d94b27bd9cd9bddce1e4239862cd147f4783ae7
0xelectron <0xproton@gmail.com>
2017-08-13 12:21:56 +0530

    Initial Docker Configuration for Deployment

--- mhtportal/wsgi.py
+++ mhtportal/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mhtportal.settings")
 
-application = get_wsgi_application()
+# To server static files from third party modules(like django rest framework)
+application = Cling(get_wsgi_application())
