437ccdd9cac30f4b1698012353048e15098697ee
0xelectron <0xproton@gmail.com>
2017-08-15 02:40:56 +0530

    Removed sms send on profile creation

--- base/signals.py
+++ base/signals.py
@@ -11,22 +11,22 @@ logger = logging.getLogger(__name__)
 
 
 
-@receiver(post_save, sender=Participant)
-def send_sms(sender, instance, created, **kwargs):
-    if created:
-        sms_string = 'Your Registration is now Complete'
+# @receiver(post_save, sender=Participant)
+# def send_sms(sender, instance, created, **kwargs):
+#     if created:
+#         sms_string = 'Your Profile is now created. You can now login.'
 
-        # Because the sms vendor auto adds 91 to the number, we'll have to remove ours
-        # Note: This is a hack and only works for India numbers. Please don't use this in
-        # production.
-        mobile = str(instance.mobile)
-        if ('+' in mobile) or ('91' in mobile[0:3]):
-            mobile = mobile[3:]
-        url = settings.SMS_URL.format(settings.SMS_USER, settings.SMS_PASS, settings.SENDER_ID, mobile, sms_string)
-        try:
-            send_sms_async.delay(url)
+#         # Because the sms vendor auto adds 91 to the number, we'll have to remove ours
+#         # Note: This is a hack and only works for India numbers. Please don't use this in
+#         # production.
+#         mobile = str(instance.mobile)
+#         if ('+' in mobile) or ('91' in mobile[0:3]):
+#             mobile = mobile[3:]
+#         url = settings.SMS_URL.format(settings.SMS_USER, settings.SMS_PASS, settings.SENDER_ID, mobile, sms_string)
+#         try:
+#             send_sms_async.delay(url)
 
-        except Exception as e:
-            logger.exception('while sending sms')
+#         except Exception as e:
+#             logger.exception('while sending sms')
 
 
