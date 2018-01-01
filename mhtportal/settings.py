f452dced69cc5b1a266888f99475490bf9b844e3
atomicnumber1 <0xproton@gmail.com>
2018-01-01 14:59:49 +0530

    Increased token expiration time and allowed tokens to refreshed

--- mhtportal/settings.py
+++ mhtportal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -270,4 +271,12 @@ LOGGING = {
             'propagate': True,
         },
     }
+}
+
+
+# Rest framework jwt settings
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
