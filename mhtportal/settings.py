87ab006e02e84caff7fa9ad763866c05ecaf1285
atomicnumber1 <0xproton@gmail.com>
2018-01-09 23:18:58 +0530

    Fixed celery config error

--- mhtportal/settings.py
+++ mhtportal/settings.py
@@ -152,8 +152,8 @@ REST_FRAMEWORK = {
 }
 
 # Celery
-CELERY_BROKER_URL = 'redis://redis:6379'
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
