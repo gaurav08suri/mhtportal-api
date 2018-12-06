3fc5b82ba07392da25ac20d2d305ec6904414df5
AT User01 <atuser01@amp.local>
2018-12-06 23:27:38 +0530

    Change in SMS vendor

--- mhtportal/settings.py
+++ mhtportal/settings.py
@@ -170,6 +170,7 @@ SMS_PASS = config('SMS_PASS')
 SENDER_ID = config('SENDER_ID')
 SMS_AUTH = config('SMS_AUTH')
 SMS_ROUTE= config('SMS_ROUTE')
+SMS_COUNTRY= "91"
 
 # Logging
 LOG_DIR = os.path.join(BASE_DIR, 'logs')
